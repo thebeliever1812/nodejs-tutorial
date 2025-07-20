453e40a9191ec1e84becd662220bb16d511b9ec3
Basir Ahmad <basirahmadmalik@gmail.com>
2025-07-21 00:27:30 +0530

    Add initial files: contacts.txt, file.js, and test.txt with example code and data

---
+++ file.js
@@ -0,0 +1,32 @@
+const fs = require("fs"); // fs stands for file system
+
+// Syncronous
+// fs.writeFileSync('./test.txt', 'This is a test file')
+
+// Asyncronous
+// fs.writeFile('./test.txt', 'This is a file', (err) => {})
+
+// Syncronous
+// const result = fs.readFileSync("./contacts.txt", "utf-8");
+// console.log(result);
+
+// Asyncronous (it doesnot return anything like the sync and this is same for fs.writeFile as well)
+// fs.readFileSync("./contacts.txt", "utf-8", (err, result) => {
+// 	if (err) {
+// 		console.log(err);
+// 	} else console.log(result);
+// });
+
+// fs.appendFileSync("./test.txt", `\nNew Text appended`)
+
+// To copy any file
+// fs.cpSync('./contacts.txt', './copy.txt')
+
+// To delete any file
+// fs.unlinkSync('./copy.txt')
+
+// console.log(fs.statSync("./test.txt"))
+
+// To create folders
+// fs.mkdirSync("my-docs");
+// fs.mkdirSync("your-docs/a/b", { recursive: true });
