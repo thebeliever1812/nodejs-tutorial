cde56328cdd1d4ab8182552f7e152ee7a23b1ac2
Basir Ahmad <basirahmadmalik@gmail.com>
2025-07-20 22:34:29 +0530

    initial commit

---
+++ math.js
@@ -0,0 +1,20 @@
+// function add(num1, num2) {
+// 	return num1 + num2;
+// }
+
+// function sub(num1, num2) {
+// 	return num1 - num2;
+// }
+
+/** Modules */
+
+// module.exports = add
+// module.exports = sub // Overrides the previous value from add to sub
+
+// module.exports = {
+// 	add,
+// 	subFn: sub,
+// };
+
+// exports.add = (a, b) => a + b; // These are anonymous funcs
+// exports.sub = (a, b) => a - b;
