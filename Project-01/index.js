6430814dbf39b65392e4dd1dae49c9304985b053
Basir Ahmad <basirahmadmalik@gmail.com>
2025-07-24 00:08:43 +0530

    Add mongoose and nodemon dependencies; update start script to use nodemon
    - Added mongoose (version 8.16.4) for MongoDB object modeling.
    - Added nodemon (version 3.1.10) for automatic server restarts during development.
    - Updated the start script in package.json to use nodemon instead of node for improved development experience.
    - Updated package-lock.json to reflect the new dependencies and their versions.

--- Project-01/index.js
+++ Project-01/index.js
@@ -1,9 +1,46 @@
 const express = require("express");
 const fs = require("fs");
-let users = require("./MOCK_DATA.json");
+const mongoose = require("mongoose");
 
 const app = express();
 const PORT = 8000;
+
+// Connect to mongoDB
+mongoose
+	.connect("mongodb://127.0.0.1:27017/node-user-app")
+	.then(() => console.log("Database connected"))
+	.catch((error) => console.log(`Error : ${error}`));
+
+// Schema
+const userSchema = new mongoose.Schema(
+	{
+		firstName: {
+			type: String,
+			required: true,
+		},
+		lastName: {
+			type: String,
+			required: false,
+		},
+		age: {
+			type: Number,
+			required: true,
+		},
+		gender: {
+			type: String,
+			required: true,
+		},
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+	},
+	{ timestamps: true }
+);
+
+// Model
+const User = mongoose.model("user", userSchema);
 
 // Middleware
 app.use(express.urlencoded({ extended: false }));
@@ -32,17 +69,24 @@ app.use((req, res, next) => {
 
 // Get all users (Make hybrid server means for both mobile and browser users)
 // For browser users, we return html doc
-app.get("/users", (req, res) => {
+app.get("/users", async (req, res) => {
+	const allUsers = await User.find({});
 	const html = `<ul>
-        ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
+        ${allUsers
+					.map(
+						(user) =>
+							`<li>Name: ${user.firstName} ${user.lastName} and Email: ${user.email}</li>`
+					)
+					.join("")}
     </ul>`;
 	return res.send(html);
 });
 // For Mobile users we return json
-app.get("/api/users", (req, res) => {
-	res.setHeader("X-MyName" , "Basir Malik") // custom response header, always add X in starting in custom header
+app.get("/api/users", async (req, res) => {
+	// res.setHeader("X-MyName", "Basir Malik"); // custom response header, always add X in starting in custom header
 	// console.log(req.headers) // gives the request headers
-	return res.json(users);
+	const allUsers = await User.find({});
+	res.status(200).json(allUsers);
 });
 
 // Get users with id
@@ -56,13 +100,28 @@ app.get("/api/users", (req, res) => {
 // });
 
 // POST create user
-app.post("/api/users", (req, res) => {
+app.post("/api/users", async (req, res) => {
 	// req.body ke andar vo data aa jaega jo hamne post request se bheja h
 	const body = req.body;
-	users.push({ id: users.length + 1, ...body });
-	fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
-		return res.json({ status: "Success", id: users.length });
+	if (
+		!body ||
+		!body.first_name ||
+		!body.last_name ||
+		!body.age ||
+		!body.gender ||
+		!body.email
+	) {
+		return res.status(400).json({ error: "All fields required" });
+	}
+	const newUser = await User.create({
+		firstName: body.first_name,
+		lastName: body.last_name,
+		age: body.age,
+		gender: body.gender,
+		email: body.email,
 	});
+
+	return res.status(201).json({ status: "User created", addedUser: newUser });
 });
 
 // // PATCH edit user
@@ -77,49 +136,48 @@ app.post("/api/users", (req, res) => {
 
 app
 	.route("/api/users/:id")
-	.get((req, res) => {
-		const id = Number(req.params.id);
-		const user = users.find((user) => user.id === id);
-		if (!user) {
-			return res.status(404).json({ error: "User not found" });
-		}
-		return res.json(user);
-	})
-	.patch((req, res) => {
-		const id = Number(req.params.id);
-		const user = users.find((user) => user.id === id);
-		if (!user) {
-			return res.json({ error: "404 not found" });
-		}
-
-		Object.assign(user, req.body);
-
-		fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err) => {
-			if (err) {
-				return res.status(500).json({ error: "Failed to update user" });
+	.get(async (req, res) => {
+		try {
+			const user = await User.findById(req.params.id);
+			if (!user) {
+				return res.status(404).json({ error: "User not found" });
 			}
-			return res.json({ status: "Success", updatedUser: user });
-		});
-	})
-	.delete((req, res) => {
-		const id = Number(req.params.id);
-		const userIndex = users.findIndex((user) => user.id === id);
-
-		if (userIndex === -1) {
-			return res.status(404).json({ error: "User not found" });
+			return res.status(200).json(user);
+		} catch (error) {
+			return res.status(500).json({ error: error.message });
 		}
-
-		const deletedUser = users.splice(userIndex, 1);
-
-		fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err) => {
-			if (err) {
-				return res.status(500).json({ status: "User not deleted" });
-			}
-			return res.json({
-				status: "User deleted successfully",
-				deletedUser: deletedUser,
+	})
+	.patch(async (req, res) => {
+		try {
+			const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+				new: true,
+				runValidators: true,
 			});
-		});
+
+			if (!user) return res.status(404).json({ error: "User not found" });
+
+			res
+				.status(200)
+				.json({ status: "User updated successfully", updatedUser: user });
+		} catch (error) {
+			console.error("Update Error:", error.message);
+			res.status(500).json({ error: "Internal Server Error" });
+		}
+	})
+	.delete(async (req, res) => {
+		try {
+			const user = await User.findByIdAndDelete(req.params.id);
+
+			if (!user) {
+				return res.status(404).json({ error: "User not found" });
+			}
+
+			res
+				.status(200)
+				.json({ status: "User deleted successfully", deletedUser: user });
+		} catch (error) {
+			res.status(500).json({ error: "Internal Server Error" });
+		}
 	});
 
 app.listen(PORT, () => console.log(`Server is running on PORT:${PORT}`));
